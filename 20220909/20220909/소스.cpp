a8b191293405222e43e4a469a815244f8c767c3f
sang9831 <tjfl9831@naver.com>
2022-09-09 14:55:31 +0900

    객체 배열 동적할당과 반환 공부 1-2

---
+++ 20220909/20220909/소스.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+using namespace std;
+
+
+class Circle {
+	int radius;
+public:
+	Circle();
+	Circle(int r);
+	~Circle();
+	void setRadius(int r) { radius = r; }
+	double getArea() { return 3.14 * radius * radius; }
+};
+
+Circle::Circle() {
+	radius = 1;
+	cout << "생성자 실행 radius = " << radius << endl;
+}
+
+Circle::Circle(int r) {
+	radius = r;
+	cout << "생성자 실행 radius = " << radius << endl;
+}
+
+Circle::~Circle() {
+	cout << "소멸자 실행 radius = " << radius << endl;
+}
+
+int main() {
+
+	Circle* pArray = new Circle[3];  //객체 배열 동적 할당
+	pArray[0].setRadius(10);
+	pArray[1].setRadius(20);
+	pArray[2].setRadius(30);
+
+	for (int i = 0; i < 3; i++) {
+		cout << pArray[i].getArea() << endl;
+	}
+
+	Circle* p = pArray;  
+	for (int i = 0; i < 3; i++) {
+		cout << p->getArea() << endl;
+		p++;
+	}
+
+	delete[] pArray;  //동적 객체배열 소멸시 소멸자는 생성순의 반대로 실행
+}
